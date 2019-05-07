75283d531ac4b2e8a110d097679c098be5c39593
Nookik <kik-si@hotmail.com>
2019-05-07 11:49:35 +0700

    Initial commit

---
+++ app/src/main/java/th/co/nookikfoodshop/MainActivity.java
@@ -0,0 +1,20 @@
+package th.co.nookikfoodshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+ //       Add Fragment to Activity
+            if (savedInstanceState == null) {
+
+                getSupportFragmentManager().beginTransaction().add(R.id.contentMainFragment, new AuthenFragment()).commit();
+            }
+    }   //Main Method
+
+}   //Main Class
