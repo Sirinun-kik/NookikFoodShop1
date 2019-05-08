82d9130863bc44116351994e71401397a5e2e99e
Nookik <kik-si@hotmail.com>
2019-05-08 15:37:56 +0700

    Read JSON Success

--- app/src/main/java/th/co/nookikfoodshop/MyConstant.java
+++ app/src/main/java/th/co/nookikfoodshop/MyConstant.java
@@ -3,6 +3,14 @@ package th.co.nookikfoodshop;
 public class MyConstant {
 
     private String urlAddUser = "https://www.androidthai.in.th/ruts/addUserkik.php";
+    private String urlGetUserWhereUser = "https://www.androidthai.in.th/ruts/getUserWhereUserkik.php";
+
+
+    public String getUrlGetUserWhereUser() {
+        return urlGetUserWhereUser;
+    }
+
+
 
     public String getUrlAddUser() {
         return urlAddUser;
